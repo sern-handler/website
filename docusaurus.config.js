70010ffe57617b8fc15aacf827645f4b0def6c80
Jacob Nguyen <76754747+jacoobes@users.noreply.github.com>
2022-08-07 10:47:25 -0500

    feat: start merge

---
+++ docusaurus.config.js
@@ -0,0 +1,137 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/nightOwlLight');
+const darkCodeTheme = require('prism-react-renderer/themes/oceanicNext');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Official sern documentation',
+  tagline: '100% api coverage',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'static/img/favicon.ico',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'sern-handler', // Usually your GitHub org/user name.
+  projectName: '@sern/handler', // Usually your repo name.
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'fr'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'sern docs',
+        logo: {
+          alt: 'My Site Logo',
+          src: 'img/favicon.ico',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'Tutorial',
+          },
+          {to: '/blog', label: 'Blog', position: 'left'},
+          {
+            href: 'https://github.com/sern-handler',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Tutorial',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Stack Overflow',
+                href: 'https://stackoverflow.com/questions/tagged/sern-handler',
+              },
+              {
+                label: 'Discord',
+                href: 'https://discord.gg/DwbF5H5JgQ',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/sern-handler',
+              },
+            ],
+          },
+        ],
+        copyright: `Built with Love ❤️`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+  plugins : [
+      [
+        'docusaurus-plugin-typedoc',
+        {
+          entryPoints: ['../sernHandlerV2/src/index.ts'],
+          tsconfig: '../sernHandlerV2/tsconfig-base.json',
+        },
+      ]
+  ]
+};
+
+module.exports = config;
---
+++ src/components/HomepageFeatures/index.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import clsx from 'clsx';
+import styles from './styles.module.css';
+
+const FeatureList = [
+  {
+    title: 'Batteries included',
+    Svg: require('@site/static/img/battery-svgrepo-com.svg').default,
+    description: (
+      <>
+        Start up a new project or integrate in minutes.
+      </>
+    ),
+  },
+  {
+    title: 'Customizable',
+    Svg: require('@site/static/img/puzzle-svgrepo-com.svg').default,
+    description: (
+      <>
+         Extend or customize with community-based plugins to provide utilities, filters, and more.
+      </>
+    ),
+  },
+  {
+    title: 'Compact',
+    Svg: require('@site/static/img/typescript-svgrepo-com.svg').default,
+    description: (
+      <>
+        Uses modern and powerful tooling such as
+          <a href={'https://swc.rs/'}> swc</a>,
+          <a href={'https://tsup.egoist.dev/'}> tsup</a>,
+          <a href={'https://www.typescriptlang.org/'}> typescript</a>, and
+          <a href={'https://rxjs.dev/'}> rxjs</a> to future-proof and ensure project quality.
+      </>
+    ),
+  },
+];
+
+function Feature({Svg, title, description}) {
+  return (
+    <div className={clsx('col col--4')}>
+      <div className="text--center">
+        <Svg className={styles.featureSvg} role="img" />
+      </div>
+      <div className="text--center padding-horiz--md">
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default function HomepageFeatures() {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
