e19b5e345e6b5fb30784e5d11c4134eaab362133
Jacob Nguyen <76754747+jacoobes@users.noreply.github.com>
2022-08-13 16:45:31 -0500

    feat: add favicon

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ const config = {
   baseUrl: '/', 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'static/img/favicon.ico', // this currently not working
+  favicon: '/img/favicon.ico', // this currently not working
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
