ae9c40ee5032507fc95b231aad9352c1a9c74557
Jacob Nguyen <76754747+jacoobes@users.noreply.github.com>
2022-08-13 14:41:38 -0500

    feat: configure docusaurus.config.js for github pages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,7 +17,9 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'sern-handler', // Usually your GitHub org/user name.
-  projectName: '@sern-handler/handler', // Usually your repo name.
+  projectName: 'website', // Usually your repo name.
+  deploymentBranch: 'master',
+  trailingSlash: false,
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
