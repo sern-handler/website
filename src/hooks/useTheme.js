5d04413dbe5aae5614044473d05982840a4760fb
Izan Gil <66965250+SrIzan10@users.noreply.github.com>
2024-04-27 20:19:22 +0200

    wow such feature

---
+++ src/hooks/useTheme.js
@@ -0,0 +1,14 @@
+/* make a react hook which checks the data-theme */
+import { useEffect, useState } from "react";
+
+export default function useTheme() {
+    const [theme, setTheme] = useState(window.localStorage.getItem('theme'));
+
+    useEffect(() => {
+        window.addEventListener('storage', (e) => {
+            e.key === 'theme' && setTheme(e.newValue);
+        })
+    }, []);
+
+    return [theme, setTheme];
+}
--- src/pages/plugins.js
+++ src/pages/plugins.js
@@ -2,17 +2,29 @@ import React from 'react';
 import Layout from '@theme/Layout';
 import clsx from "clsx";
 import styles from "./index.module.css";
+import PluginCard from "../components/PluginCard";
 
 export default function Plugins() {
+    const [pluginList, setPluginList] = React.useState([])
+    React.useEffect(() => {
+        const asyncFetch = async () => {
+            const response = await fetch('https://raw.githubusercontent.com/sern-handler/awesome-plugins/main/pluginlist.json')
+            const data = await response.json()
+            setPluginList(data)
+        }
+        asyncFetch()
+    }, [])
 
     return (
         <Layout>
-            <header className={clsx('hero hero--primary-darker', styles.heroBanner)}>
-                <h1 className={"hero__title"}>
-                    Coming soon
-                </h1>
-            </header>
-
+            <h1 className={styles.header}>
+                Plugins
+            </h1>
+            <div className={styles.cntnr}>
+                {pluginList.map(p => (
+                    <PluginCard plugin={p} key={p.hash} />
+                ))}
+            </div>
         </Layout>
     )
 }
