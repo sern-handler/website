c13abec8e08d5af1b3ed54b8d943fd776fe8d711
DuroCodes <davidwright13503@gmail.com>
2024-06-10 23:50:17 -0400

    fix: await bun shell call for tools in `setup.mjs`

--- setup.mjs
+++ setup.mjs
@@ -1,6 +1,6 @@
 import { $ } from "bun";
 import { GITHUB_URL } from '~/utils/consts.ts';
-import { existsSync } from 'node:fs'
+import { existsSync } from 'node:fs';
 import { copyFile } from "node:fs/promises";
 
 const gits = [
@@ -20,17 +20,17 @@ for (const git of gits) {
 }
 
 const tools = ['poster', 'ioc', 'builder', 'localizer', 'publisher'];
-$`bunx --yes degit --force sern-handler/tools/packages tools/`
-    
-await Promise.all(tools.map(tool =>  { 
-    const docpage = `./tools/${tool}/index.mdx`;
-    const metadata = `./tools/${tool}/metadata.json`;
-    if(existsSync(docpage) && existsSync(metadata)) {
-        console.log('cp', docpage, './src/content/docs/v4/tools/'+tool+'.mdx')
-        return copyFile(docpage, './src/content/docs/v4/tools/'+tool+'.mdx')
-    } else {
-        console.warn(docpage, 'or', metadata, 'not found for', tool)
-    }
+await $`bunx --yes degit --force sern-handler/tools/packages tools/`;
+
+await Promise.all(tools.map((tool) => {
+  const docpage = `./tools/${tool}/index.mdx`;
+  const metadata = `./tools/${tool}/metadata.json`;
+  if (existsSync(docpage) && existsSync(metadata)) {
+    console.log(`cp ${docpage} ./src/content/docs/v4/tools/${tool}.mdx`);
+    return copyFile(docpage, `./src/content/docs/v4/tools/${tool}.mdx`);
+  } else {
+    console.warn(`${docpage} or ${metadata} not found for ${tool}`);
+  }
 }));
 
 
