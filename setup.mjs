3907687e2bb2464c4e87f68b4bfc81e242d49bc6
DuroCodes <davidwright13503@gmail.com>
2024-06-10 23:59:15 -0400

    fix: remove `bunx degit ...` so it builds

--- setup.mjs
+++ setup.mjs
@@ -20,7 +20,7 @@ for (const git of gits) {
 }
 
 const tools = ['poster', 'ioc', 'builder', 'localizer', 'publisher'];
-await $`bunx --yes degit --force sern-handler/tools/packages tools/`;
+// await $`bunx --yes degit --force sern-handler/tools/packages tools/`;
 
 await Promise.all(tools.map((tool) => {
   const docpage = `./tools/${tool}/index.mdx`;
