6329c6802a43a1cc319198cfd4a44968045643ab
Jacob Nguyen <76754747+jacoobes@users.noreply.github.com>
2024-07-18 16:13:59 -0500

    deploy again

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,7 +13,7 @@ const [typeDocV4, typeDocV4Sidebar] = createStarlightTypeDocPlugin();
 // https://astro.build/config
 export default defineConfig({
   // TODO: Change this whenever site is deployed to `sern.dev`
-  site: 'https:/deploy-preview-66--sern-docs.netlify.app',
+  site: 'sern.dev',
   integrations: [starlight({
     title: "sern",
     lastUpdated: true,
